52c671f1e13729d271c9226b0383a3f77f5e6b2b
Jason Kim <95jasonkim@gmail.com>
2021-03-30 11:01:21 -0800

    Github test edit by Jason

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "CS202 Group Project with Travis, Michael, Galen, and Jason" << std::endl;
-
+    std::cout << "Test edit by Jason." << std::endl;
     return 0;
 }
