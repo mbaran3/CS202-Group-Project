0c45ada53949e7b377b6dbc9cbaef53e49fa23ae
mbaran3 <mbaran3@alaska.edu>
2021-03-30 10:47:44 -0800

    First commmit

---
+++ cmake-build-debug/CMakeFiles/CS202_Group_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS202_Group_Project.dir/main.cpp.obj"
+  "CS202_Group_Project.exe"
+  "CS202_Group_Project.exe.manifest"
+  "CS202_Group_Project.lib"
+  "CS202_Group_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS202_Group_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "CS202 Group Project with Travis, Michael, Galen, and Jason" << std::endl;
+
+    return 0;
+}
