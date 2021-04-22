42f9e422d7b05c59cb787d9c2a635558fd8859ee
Galen Heninger <heninger.galen@gmail.com>
2021-04-21 16:40:43 -0800

    ClassName() overrides now marked 'override'

--- Character_Class.h
+++ Character_Class.h
@@ -10,63 +10,63 @@ class Character_Class {
 public:
 
     virtual std::string className() {return "No class set";}
-	virtual bool requiredSkill(PC& player) const = 0;
-	
+    virtual bool requiredSkill(PC& player) const = 0;
+    
 };
 
 //Classes for Player character to choose from, derived from Character_Class
 class Apothecary : public Character_Class{
 
 public:
-    std::string className() {return "Apothecary";}
+    std::string className() override {return "Apothecary";}
     bool requiredSkill(PC& player) const override;
-	static std::string description;
+    static std::string description;
 };
 
 
 class Bard : public Character_Class{
 
 public:
-    std::string className()  {return "Bard";}
-	static std::string description;     
-	bool requiredSkill(PC& player) const override;
+    std::string className() override {return "Bard";}
+    static std::string description;
+    bool requiredSkill(PC& player) const override;
 
 };
 
 class Brawler : public Character_Class{
 public:
-    std::string className() {return "Brawler";}
+    std::string className() override {return "Brawler";}
     bool requiredSkill(PC& player) const override;
-	static std::string description;
+    static std::string description;
 };
 
 
 class Knight : public Character_Class {
 public:
-    std::string className() {return "Knight";}
+    std::string className() override {return "Knight";}
     bool requiredSkill(PC& player) const override;
-	static std::string description;
+    static std::string description;
 };
 
 class Sage : public Character_Class {
 public:
-    std::string className() {return "Sage";}
+    std::string className() override {return "Sage";}
     bool requiredSkill(PC& player) const override;
-	static std::string description; 
+    static std::string description;
 };
 
 class Thief : public Character_Class {
 public:
-    std::string className() {return "Thief";}
+    std::string className() override {return "Thief";}
     bool requiredSkill(PC& player) const override;
-	static std::string description; 
+    static std::string description;
 };
 
 class Wildlander : public Character_Class {
 public:
-    std::string className() {return "Wildlander";}
+    std::string className() override {return "Wildlander";}
     bool requiredSkill(PC& player) const override;
-	static std::string description;
+    static std::string description;
 };
 
 #endif //CS202_GROUP_PROJECT_CHARACTER_CLASS
